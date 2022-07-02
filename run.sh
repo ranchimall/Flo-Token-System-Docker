c048a31bbf90be53ef2eb3c8b5e93db7fcf531d5
Divyansh Bhardwaj <87605149+Dbhardwaj99@users.noreply.github.com>
2022-07-03 01:09:06 +0530

    Changes in run.sh for floscout APu url

--- run.sh
+++ run.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 cd /etc/supervisor/conf.d/
 
-sed -i "s|window.tokenapiUrl = 'http://0.0.0.0:6012'|window.tokenapiUrl = '$FLOAPIURL'|" /floscout/index.html
-sed -i "s|command=hypercorn -w 1 -b 0.0.0.0:6012 wsgi:app|command=hypercorn -w 1 -b $FLOAPIURL wsgi:app" /etc/supervisor/conf.d/ftt-ranchimallflo.conf
+FLOAPIURL = $(FLOAPIURL)
+NETWROKK = $(NETWROKK)
 
-#cat /floscout/index.html
+sed -i "s|window.tokenapiUrl = 'http://0.0.0.0:6012'|window.tokenapiUrl = '$FLOAPIURL'|" /floscout/index.html
+#sed -i "s|command=hypercorn -w 1 -b 0.0.0.0:6012 wsgi:app|command=hypercorn -w 1 -b $FLOAPIURL wsgi:app" /etc/supervisor/conf.d/ftt-ranchimallflo.conf
+
+cat /floscout/index.html
 #cat /etc/supervisor/conf.d/ftt-ranchimallflo.conf
 
+echo $FLOAPIURL
+echo $NETWROKK
 echo "running..."
 supervisord -c /etc/supervisor/conf.d/ftt-ranchimallflo.conf
 #./floscout/example
