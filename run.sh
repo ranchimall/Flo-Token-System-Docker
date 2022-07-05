f5a6851c733fdaf70812d446c9c40335873f3cd6
Divyansh Bhardwaj <87605149+Dbhardwaj99@users.noreply.github.com>
2022-07-05 22:29:55 +0530

    Changed the binary Added commands for docker volume creation

--- run.sh
+++ run.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 cd /etc/supervisor/conf.d/
 
+sed -i "s|command=hypercorn -w 1 -b 0.0.0.0:6012 wsgi:app|command=hypercorn -w 1 -b $FLOAPIURL wsgi:app|" /etc/supervisor/conf.d/ftt-ranchimallflo.conf
 sed -i "s|window.tokenapiUrl = 'https://ranchimallflo.duckdns.org'|window.tokenapiUrl = '$FLOAPIURL'|" /floscout/index.html
-#sed -i "s|command=hypercorn -w 1 -b 0.0.0.0:6012 wsgi:app|command=hypercorn -w 1 -b $FLOAPIURL wsgi:app" /etc/supervisor/conf.d/ftt-ranchimallflo.conf
-
-#cat /floscout/index.html
-#cat /etc/supervisor/conf.d/ftt-ranchimallflo.conf
 
 echo "running..."
 #supervisord -c /etc/supervisor/conf.d/ftt-ranchimallflo.conf
